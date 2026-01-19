a545f52cccf12543f067e76ae489cf3346efb194
Andrew <andrewmouchantaf@gmail.com>
2026-01-19 15:02:10 -0500

    Slight verbiage update to be more accurate

--- src/ethernet.c
+++ src/ethernet.c
@@ -7,13 +7,13 @@ static void print_mac_address(unsigned char* bytes) {
     printf("%02x:%02x:%02x:%02x:%02x:%02x", bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5]); 
 }
 
-int parse_ethernet_header(const unsigned char* packet, size_t len, ethresult_t* out) {
+int parse_ethernet_header(const unsigned char* frame, size_t len, ethresult_t* out) {
     if (len < sizeof(struct ethhdr)) {
-        fprintf(stderr, "Packet too small to contain ethernet header, continuing\n");
+        fprintf(stderr, "Frame too small to contain ethernet header, continuing\n");
         return -1;
     }
 
-    struct ethhdr *ethernet_header = (struct ethhdr *)packet;
+    struct ethhdr *ethernet_header = (struct ethhdr *)frame;
     printf("   Ethernet host destination: ");
     print_mac_address(ethernet_header->h_dest);
     printf("\n");
@@ -24,7 +24,7 @@ int parse_ethernet_header(const unsigned char* packet, size_t len, ethresult_t* 
     switch(ntohs(ethernet_header->h_proto)) {
         case(ETH_P_IP):
             out->ethertype = ETHERTYPE_IP;
-            out->payload = packet + ETH_HLEN;
+            out->payload = frame + ETH_HLEN;
             out->payload_len = len - ETH_HLEN;
             return 0;
         case(ETH_P_IPV6):
